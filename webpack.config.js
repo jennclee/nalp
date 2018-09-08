b0b98b01571e920eef3076558d8daadd514eed95
jennclee <8753352+jennclee@users.noreply.github.com>
2018-09-07 18:11:20 -0700

    Update webpack configs for ES6 syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,28 @@
 const path = require('path')
 
-const sourcePath = path.join(__dirname, 'client/src')
-const outputPath = path.join(__dirname, 'client/dist')
-
-module.exports.config = {
+const config = {
   entry: './client/src/app.js',
   output: {
-    path: outputPath,
+    path: path.resolve(__dirname, 'client/dist'),
     filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: 'babel-loader',
         exclude: /node_modules/,
+        loader: 'babel-loader',
         options: {
-          presets: ['react'],
+          presets: [
+            ['es2016'], 'react',
+          ],
+          plugins: [
+            'babel-plugin-transform-class-properties',
+          ],
         },
       },
     ],
   },
-  resolve: {
-    extensions: ['.js'],
-    modules: [
-      path.resolve(__dirname, 'node_modules'),
-      sourcePath,
-    ],
-  },
 }
+
+module.exports = config
